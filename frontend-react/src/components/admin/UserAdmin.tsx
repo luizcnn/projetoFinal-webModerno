745051fd68fe364e2286ba72466f6f25f4495a3c
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2020-12-10 20:41:10 -0300

    UserAdmin refatorado tipagem em state

--- frontend-react/src/components/admin/UserAdmin.tsx
+++ frontend-react/src/components/admin/UserAdmin.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import {FaTrash, FaPen} from 'react-icons/fa'
 import { Button, Col, Form, Table } from 'react-bootstrap';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 import api from '../../services/api';
+import { notifySuccess, notifyError } from '../../utils/toastify'
 
 import './styles/userAdmin.css'
 import 'react-toastify/dist/ReactToastify.css';
@@ -19,71 +20,49 @@ interface User {
 
 function UserAdmin() {
     const [saveModeToggle, setSaveModeToggle] = useState(true)
+    const defaultUser = {
+        id: 0,
+        name: '',
+        email: '',
+        admin: false,
+        password: '',
+        confirmPassword: ''
+    }
     
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [admin, setAdmin] = useState(false)
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [id, setId] = useState(0)
+    const [user, setUser] = useState<User>({ ...defaultUser })
     const [users, setUsers] = useState([])
 
     function resetForm() {
-        setName('')
-        setEmail('')
-        setId(0)
-        setPassword('')
-        setConfirmPassword('')
-        setAdmin(false)
+        setUser({ ...defaultUser })
+
         setSaveModeToggle(true)
     }
 
     function saveUser() {
-        
-        const idForm = id !== 0 ? `/${id}` : ''
-        const method = id !==0 ? 'put' : 'post'
-        
-        if(method === 'post') {
-            api.post('users', {
-                name,
-                email,
-                admin,
-                password,
-                confirmPassword
-            }).then(() => {
-                notifySuccess("Cadastro realizado com sucesso")
-            }).catch((err) => {
-                if(err.response) {
-                    notifyError(err.response.data)
-                }
-            })
-        } else {
-            api.put(`users${idForm}`, {
-                name,
-                email,
-                admin,
-                password,
-                confirmPassword
-            }).then(() => {
-                notifySuccess("Edição realizada com sucesso")
-            }).catch((err) => {
-                if(err.response) {
-                    notifyError(err.response.data)
-                }
-            })
-        }
+         
+        const idForm = user.id !== 0 ? `/${user.id}` : ''
+        const method = user.id !==0 ? 'put' : 'post'
 
+        api[method](`users${idForm}`, user)
+            .then(() => {
+                notifySuccess("Cadastro realizado com sucesso")
+            }).catch((error) => {
+                if(error.response) {
+                    notifyError(error.response.data)
+                }
+            })
+        
         resetForm()
         
     }
 
     function removeUser() {
-        api.delete(`users/${id}`)
+        api.delete(`users/${user.id}`)
             .then(() => {
                 notifySuccess("Remoção realizada com sucesso")
-            }).catch((err) => {
-                if(err.response) {
-                    notifyError(err.response.data)
+            }).catch((error) => {
+                if(error.response) {
+                    notifyError(error.response.data)
                 }
             })
         
@@ -92,12 +71,14 @@ function UserAdmin() {
 
     function loadUser(user: User, saveMode = true) {
         setSaveModeToggle(saveMode);
-        setName(user.name);
-        setEmail(user.email);
-        setAdmin(user.admin);
-        setId(user.id);
-        setPassword('')
-        setConfirmPassword('')
+        setUser({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            admin: user.admin,
+            password: '',
+            confirmPassword: ''
+        })
     }
 
     function loadUsers() {
@@ -105,26 +86,6 @@ function UserAdmin() {
             .then(res => {
                 setUsers(res.data)
             })
-    }
-
-    function notifySuccess(msg: string) {
-        toast.success(msg, {
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            progress: undefined,            
-        })
-    }
-
-    function notifyError(msg: string) {
-        toast.error(msg, {
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            progress: undefined
-        })
     }
 
     useEffect(() => {
@@ -137,7 +98,7 @@ function UserAdmin() {
             <Form onSubmit={(e) => {
                 e.preventDefault()
             }}>
-                <input id="article-id" type="hidden" name="article-id" value={id}/>
+                <input id="article-id" type="hidden" name="article-id" value={user.id}/>
                 <Form.Row>
                     <Col sm={12} md={6}>
                         <Form.Group controlId="form-name">
@@ -145,8 +106,8 @@ function UserAdmin() {
                             <Form.Control 
                                 type="text" 
                                 placeholder="Informe o Nome do Usuário" 
-                                value={name}
-                                onChange={(e) => {setName(e.target.value)}}
+                                value={user.name}
+                                onChange={(e) => {setUser({...user, name: e.target.value})}}
                                 readOnly={!saveModeToggle}
                             />
                         </Form.Group>
@@ -157,8 +118,8 @@ function UserAdmin() {
                         <Form.Control 
                             type="email" 
                             placeholder="Informe o E-mail do Usuário" 
-                            value={email}
-                            onChange={(e) => {setEmail(e.target.value)}}
+                            value={user.email}
+                            onChange={(e) => {setUser({...user, email: e.target.value})}}
                             readOnly={!saveModeToggle}
                         />
                     </Form.Group>
@@ -168,9 +129,9 @@ function UserAdmin() {
                     <Form.Check 
                         id="user-admin"
                         className="mt-3 mb-3"
-                        checked={admin}
+                        checked={user.admin}
                         label="Administrador"
-                        onChange={() => setAdmin(!admin)}
+                        onChange={(e) => {setUser({...user, admin: !user.admin})}}
                         readOnly={!saveModeToggle}
                     />
                 )}
@@ -182,8 +143,8 @@ function UserAdmin() {
                                 <Form.Control 
                                     type="password" 
                                     placeholder="Informe a Senha" 
-                                    value={password}
-                                    onChange={(e) => {setPassword(e.target.value)}}
+                                    value={user.password}
+                                    onChange={(e) => {setUser({...user, password: e.target.value})}}
                                 />
                             </Form.Group>
                         </Col>
@@ -193,8 +154,8 @@ function UserAdmin() {
                             <Form.Control 
                                 type="password" 
                                 placeholder="Confirme a Senha do Usuário" 
-                                value={confirmPassword}
-                                onChange={(e) => {setConfirmPassword(e.target.value)}}
+                                value={user.confirmPassword}
+                                onChange={(e) => {setUser({...user, confirmPassword: e.target.value})}}
                             />
                         </Form.Group>
                         </Col>
